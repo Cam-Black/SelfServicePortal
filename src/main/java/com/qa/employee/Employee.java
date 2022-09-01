02326ea91d8efbafd4d6cd4a00415d14e2ca8250
Cam-Black <cameronablack1998@gmail.com>
2022-09-01 13:40:37 +0100

    Added getters and setters and overridden toString() method.

--- src/main/java/com/qa/employee/Employee.java
+++ src/main/java/com/qa/employee/Employee.java
@@ -19,4 +19,55 @@ public class Employee {
 		this.age = age;
 		this.salary = salary;
 	}
+	
+	public int getEmployeeId() {
+		return employeeId;
+	}
+	
+	public void setEmployeeId(int employeeId) {
+		this.employeeId = employeeId;
+	}
+	
+	public String getFirstName() {
+		return firstName;
+	}
+	
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+	public String getLastName() {
+		return lastName;
+	}
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public int getAge() {
+		return age;
+	}
+	
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public double getSalary() {
+		return salary;
+	}
+	
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [" +
+				"employeeId: " + this.getEmployeeId() +
+				", firstName: " + this.getFirstName() +
+				", lastName: " + this.getLastName() +
+				", age: " + this.getAge() +
+				", salary: " + this.getSalary() +
+				"]";
+	}
 }
