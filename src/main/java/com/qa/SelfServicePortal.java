8644b40299f5907469278df0afb3c10bbdd4cb23
Cam-Black <cameronablack1998@gmail.com>
2022-09-01 16:25:55 +0100

    Moved functionality of the portal into it's own class to clean up main method.

---
+++ src/main/java/com/qa/SelfServicePortal.java
@@ -0,0 +1,28 @@
+package com.qa;
+
+import com.qa.controller.EmployeeController;
+import com.qa.utils.Utils;
+
+public class SelfServicePortal {
+	public void portal() {
+		boolean stop = false;
+		while (!stop) {
+			Utils utils = new Utils();
+			EmployeeController employeeController = new EmployeeController(utils);
+			System.out.println("What would you like to do?");
+			System.out.println("1 - Create a new Employee");
+			System.out.println("0 - Exit");
+			int option = utils.getInt();
+			switch (option) {
+				case 0:
+					stop = true;
+					break;
+				case 1:
+					employeeController.createEmployee();
+					break;
+				default:
+					System.out.println("Please enter a valid number!");
+			}
+		}
+	}
+}
