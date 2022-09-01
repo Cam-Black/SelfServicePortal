fe9bb57951268ff2f8e64aa34de2c6fd94ee8228
Cam-Black <cameronablack1998@gmail.com>
2022-09-01 16:26:58 +0100

    Cleaned up main method, creating an instance of the selfserviceportal object and calling it in main, instead of writing all of the functionality inside main.

--- src/main/java/com/qa/Runner.java
+++ src/main/java/com/qa/Runner.java
@@ -1,26 +1,8 @@
 package com.qa;
 
-import com.qa.controller.EmployeeController;
-import com.qa.entity.Employee;
-import com.qa.utils.Utils;
-
-import java.util.Scanner;
-
 public class Runner {
 	public static void main(String[] args) {
-		boolean stop = false;
-		while (!stop) {
-			Utils utils = new Utils();
-			EmployeeController employeeController = new EmployeeController(utils);
-			System.out.println("What would you like to do?");
-			System.out.println("1 - Create a new Employee");
-			System.out.println("0 - Exit");
-			int option = utils.getInt();
-			if (option == 1) {
-				System.out.println(employeeController.createEmployee());
-			} else if (option == 0) {
-				stop = true;
-			}
-		}
+		SelfServicePortal ssp = new SelfServicePortal();
+		ssp.portal();
 	}
 }
