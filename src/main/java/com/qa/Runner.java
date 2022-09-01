1bc1ab226037c289da2c8bbc0427572432142cea
Cam-Black <cameronablack1998@gmail.com>
2022-09-01 14:22:13 +0100

    Added Scanner to create an employee if user chooses create.

--- src/main/java/com/qa/Runner.java
+++ src/main/java/com/qa/Runner.java
@@ -1,7 +1,19 @@
 package com.qa;
 
+import com.qa.controller.EmployeeController;
+import com.qa.entity.Employee;
+
+import java.util.Scanner;
+
 public class Runner {
 	public static void main(String[] args) {
-	
+		EmployeeController employeeController = new EmployeeController();
+		Scanner s = new Scanner(System.in);
+		System.out.println("What would you like to do?");
+		System.out.println("1 - Create a new Employee");
+		int input = s.nextInt();
+		if (input == 1) {
+			System.out.println(employeeController.createEmployee());
+		}
 	}
 }
