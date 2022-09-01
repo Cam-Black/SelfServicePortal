9abfb96b41db42ab5ec0de780bb65acfc7038ed5
Cam-Black <cameronablack1998@gmail.com>
2022-09-01 14:23:20 +0100

    Added default constructor along with create method.

--- src/main/java/com/qa/controller/EmployeeController.java
+++ src/main/java/com/qa/controller/EmployeeController.java
@@ -1,4 +1,13 @@
 package com.qa.controller;
 
+import com.qa.entity.Employee;
+
 public class EmployeeController {
+	public EmployeeController() {
+		super();
+	}
+	
+	public Employee createEmployee() {
+		return new Employee();
+	}
 }
