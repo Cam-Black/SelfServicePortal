aff96bf7b953c629cb119adf5b337c04989c2059
Cam-Black <cameronablack1998@gmail.com>
2022-09-01 13:35:51 +0100

    Created default constructor and constructor with all fields passed.

--- src/main/java/com/qa/employee/Employee.java
+++ src/main/java/com/qa/employee/Employee.java
@@ -6,4 +6,17 @@ public class Employee {
 	private String lastName;
 	private int age;
 	private double salary = 20000;
+	
+	public Employee() {
+		super();
+	}
+	
+	public Employee(int employeeId, String firstName, String lastName, int age, double salary) {
+		super();
+		this.employeeId = employeeId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.salary = salary;
+	}
 }
